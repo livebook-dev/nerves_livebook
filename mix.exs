36bceafbcb818a29f246540b495a4da7722b8072
Frank Hunleth <fhunleth@troodon-software.com>
2021-05-07 22:17:09 -0400

    Experiment with Mix.install

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule NervesLivebook.MixProject do
   def application do
     [
       mod: {NervesLivebook.Application, []},
-      extra_applications: [:logger, :runtime_tools, :inets, :ex_unit]
+      extra_applications: [:logger, :mix, :hex, :runtime_tools, :inets, :ex_unit]
     ]
   end
 
